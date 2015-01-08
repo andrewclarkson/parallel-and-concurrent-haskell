bafc9c50fb7917aa806745bc5bf34b1209d7faa9
Andrew Clarkson <andrew@clarkson.mn>
2015-01-08 15:00:16 -0600

    Parameterized version

--- eval-strategies/strat.hs
+++ eval-strategies/strat.hs
@@ -3,12 +3,15 @@ import Control.Parallel.Strategies hiding (parPair)
 fib 0 = 1
 fib 1 = 1
 fib x = fib (x - 1) + fib (x - 2)
+
+evalPair :: Strategy a -> Strategy b -> Strategy (a,b)
+evalPair sa sb (a,b) = do
+    a' <- sa a
+    b' <- sb b
+    return (a', b')
  
 parPair :: Strategy (a, b)
-parPair (a, b) = do
-    a' <- rpar a
-    b' <- rpar b
-    return (a', b')
+parPair = evalPair rpar rpar
 
 main :: IO ()
 main = do
