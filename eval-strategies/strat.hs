3e1082c2e25ffdab92288a59c1d2e24049a519dd
Andrew Clarkson <andrew@clarkson.mn>
2015-01-08 14:48:07 -0600

    Basic evaluation strategy

---
+++ eval-strategies/strat.hs
@@ -0,0 +1,19 @@
+import Control.Parallel.Strategies hiding (parPair)
+
+fib 0 = 1
+fib 1 = 1
+fib x = fib (x - 1) + fib (x - 2)
+ 
+parPair :: Strategy (a, b)
+parPair (a, b) = do
+    a' <- rpar a
+    b' <- rpar b
+    return (a', b')
+
+
+main :: IO ()
+main = do
+    let pair = runEval $ parPair (fib 35, fib 36)
+    print(pair)
+    return ()
+
