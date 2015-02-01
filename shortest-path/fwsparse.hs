100f872277dad757446510b8cb1b9c64e2f555ff
Andrew Clarkson <andrew@clarkson.mn>
2015-02-01 10:40:12 -0600

    Factor out closures + better printing

--- shortest-path/fwsparse.hs
+++ shortest-path/fwsparse.hs
@@ -5,25 +5,33 @@ import Data.IntMap.Strict (IntMap)
 
 import Graph
 
-shortestPaths :: [Vertex] -> Graph -> Graph
-shortestPaths vs g = foldl' update g vs
+update :: [Vertex] -> Graph -> Int -> Graph
+update vs g k = Map.mapWithKey shortmap' g
     where
-        update g k = Map.mapWithKey shortmap g
-            where
-                shortmap :: Vertex -> IntMap Weight -> IntMap Weight
-                shortmap i jmap = foldr shortest Map.empty vs 
-                    where
-                        shortest j m = case (old, new) of
-                            (Nothing, Nothing) -> m
-                            (Nothing, Just w ) -> Map.insert j w m 
-                            (Just w , Nothing) -> Map.insert j w m 
-                            (Just w1, Just w2) -> Map.insert j (min w1 w2) m
-                            where
-                                old = Map.lookup j jmap
-                                new = do 
-                                    w1 <- weight g i k
-                                    w2 <- weight g k j 
-                                    return (w1 + w2) 
+        shortmap' = shortmap vs g k
+
+shortmap :: [Vertex] -> Graph -> Int -> Vertex -> IntMap Weight -> IntMap Weight
+shortmap vs g k i jmap = foldr shortest' Map.empty vs
+    where
+        shortest' = shortest jmap g k i
+
+shortest :: IntMap Weight -> Graph -> Int -> Vertex -> Int -> IntMap Weight -> IntMap Weight
+shortest jmap g k i j m = case (old, new) of
+    (Nothing, Nothing) -> m
+    (Nothing, Just w ) -> Map.insert j w m 
+    (Just w , Nothing) -> Map.insert j w m 
+    (Just w1, Just w2) -> Map.insert j (min w1 w2) m
+    where
+        old = Map.lookup j jmap
+        new = do 
+            w1 <- weight g i k
+            w2 <- weight g k j 
+            return (w1 + w2) 
+
+shortestPaths :: [Vertex] -> Graph -> Graph
+shortestPaths vs g = foldl' update' g vs
+    where
+        update' = update vs
         
 test :: [[Int]]
 test  = [[  0, 999, 999,  13, 999, 999],
@@ -38,8 +46,10 @@ mkGraph xss = Map.fromList (zipWith row [0..] xss)
   where
    row i xs = (i, Map.fromList [ (j, w) | (j,w) <- zip [0..] xs, w /= 100 ])
 
+debugMap v = map (\(_, x) -> x) (Map.toList v)
+
 main :: IO ()
 main = do
     let
         shortest = shortestPaths [1..5] (mkGraph test)
-    print shortest
+    print (map debugMap (Map.elems shortest))
