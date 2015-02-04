d45588196f0a5032dfa18ba38616e8b18ff4460b
Andrew Clarkson <andrew@clarkson.mn>
2015-02-03 22:56:16 -0600

    Add benchmark code

--- timetable/timetable.hs
+++ timetable/timetable.hs
@@ -33,3 +33,28 @@ generate slotNo trackNo slots slot slotTalks talks
         ]
 
 
+bench :: Int -> Int -> Int -> Int -> Int -> StdGen
+      -> ([Person],[Talk],[TimeTable])
+
+bench nslots ntracks ntalks npersons c_per_s gen =
+  (persons,talks, timetable persons talks ntracks nslots)
+ where
+  total_talks = nslots * ntracks
+
+  talks = map Talk [1..total_talks]
+  persons = mkpersons npersons gen
+
+  mkpersons :: Int -> StdGen -> [Person]
+  mkpersons 0 g = []
+  mkpersons n g = Person ('P':show n) (take c_per_s cs) : rest
+        where
+          (g1,g2) = split g
+          rest = mkpersons (n-1) g2
+          cs = nub [ talks !! n | n <- randomRs (0,ntalks-1) g ]
+
+main = do
+   [ a, b, c, d, e ] <- fmap (fmap read) getArgs
+   let g = mkStdGen 1001
+   let (ss,cs,ts) = bench a b c d e g
+   print ss
+   print (length ts)
